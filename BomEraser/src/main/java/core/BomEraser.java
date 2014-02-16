4f7086e4cdc8a151f71a8ebadbbf6b3f6ae059dd
Tanabe <6isstrong@gmail.com>
2014-02-16 13:17:07 +0900

    BOMを除去する処理を実装
    BOM除去処理を実装。

--- BomEraser/src/main/java/core/BomEraser.java
+++ BomEraser/src/main/java/core/BomEraser.java
@@ -1,6 +1,11 @@
 package core;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * 指定ファイルからBomを除去します。
@@ -42,19 +47,114 @@ public class BomEraser {
     }
     
     /**
-     * 指定ファイルからBomを除去します。
-     * @param file 指定ファイル
-     * @return Bomを除去したファイルの出力先。指定ファイルが存在しない場合はnull。
+     * 入力ファイルからBomを除去したファイルを、指定の出力先に出力します。
+     * @param file 入力ファイル
+     * @param output 出力先
+     * @return 処理結果
+     * <ul>
+     *   <li>true:出力成功</li>
+     *   <li>false:出力失敗</li>
+     * </ul>
      * @throws IllegalArgumentException 引数がNullの場合
+     * @throws IOException 処理中に例外が発生した場合
      */
-    public File erase(File file) {
+    public boolean erase(File input, File output) throws IOException {
+        if(input == null) {
+            throw new IllegalArgumentException("入力ファイルがnull");
+        }
+        if(output == null) {
+            throw new IllegalArgumentException("出力先がnull");
+        }
+            
+        if(!input.exists()) {
+            return false;
+        }
+        if(output.exists()) {
+            return false;
+        }
+        
+        try (
+            BufferedInputStream inputStream = this.createSkipBOMStream(input);
+            FileOutputStream outputStream = new FileOutputStream(output);
+        ) {
+            int data;
+            while((data = inputStream.read()) > 0) {
+                outputStream.write(data);
+            }
+            return true;
+        }
+        catch (FileNotFoundException e) {
+            throw new IOException(e);
+        }
+    }
+    
+    /**
+     * BOMを読み飛ばすStreamを作成します。
+     * @param file 読み取り対象のファイル
+     * @return BOMを読み飛ばすStream
+     * @throws IOException Streamの生成に失敗した場合
+     */
+    public BufferedInputStream createSkipBOMStream( File file ) throws IOException {
         if(file == null) {
             throw new IllegalArgumentException("引数がnull");
         }
-        if(!file.exists()) {
-            return null;
+        
+        BufferedInputStream stream = null;
+        try {
+            stream = new BufferedInputStream(
+                        new FileInputStream(file));
+            
+            if(stream.available() >= 3) {
+                byte[] buffer = {0,0,0};
+                stream.read(buffer, 0, 3);
+                if(this.isBom(buffer)) {
+                    // 読み込み位置をBOMの直後に修正
+                    stream.mark(3);
+                }
+            }
+            
+            return stream;
+        }
+        catch(FileNotFoundException e) {
+            if(stream != null) {
+                stream.close();
+            }
+            throw new IOException(e);
+        }
+    }
+    
+    /**
+     * 指定配列の先頭がBOMを判定します。
+     * @param target 指定配列
+     * @return 判定結果
+     * <ul>
+     *  <li>true:指定配列の先頭がBOMである</li>
+     *  <li>false:指定配列の先頭がBOMではない</li>
+     * </ul>
+     * @throws IllegalAccessException 引数がnullの場合
+     */
+    public boolean isBom(byte[] target) {
+        if(target == null) {
+            throw new IllegalArgumentException("引数がnull");
         }
         
-        return null;
+        if(target.length < 3) {
+            return false;
+        }
+        
+        if( target[0] != (byte)0xEF ) {
+            return false;
+        }
+        
+        if(target[1] != (byte)0xBB ) {
+            return false;
+        }
+        
+        if(target[2] != (byte)0xBF ) {
+            return false;
+        }
+        
+        return true;
     }
 }
+
--- BomEraser/src/test/java/core/BomEraserTest.java
+++ BomEraser/src/test/java/core/BomEraserTest.java
@@ -3,7 +3,9 @@ package core;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.io.IOException;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
@@ -16,6 +18,7 @@ import org.junit.runner.RunWith;
  */
 @RunWith(Enclosed.class)
 public class BomEraserTest {
+
     /**
      * BomEraserのEraseメソッド用テストクラス
      * @author tanabe
@@ -29,26 +32,109 @@ public class BomEraserTest {
         private BomEraser target;
         
         @Before
-        public void before()
-        {
+        public void before() {
+            target = new BomEraser();
+        }
+        
+        @After
+        public void after() {
+            File file = this.getNotExistingFile();
+            if(file.exists()) {
+                // 出力されたファイルを削除
+                file.delete();
+            }
+        }
+        
+        @Test(expected = IllegalArgumentException.class)
+        public void 入力ファイルがNullだと例外発生() throws IOException {
+            this.target.erase(null,
+                              this.getExistingFile());
+        }
+
+        @Test(expected = IllegalArgumentException.class)
+        public void 出力ファイルがNullだと例外発生() throws IOException {
+            this.target.erase(this.getExistingFile(),
+                              null);
+        }
+        
+        @Test
+        public void 入力ファイルが存在しないファイルだと戻り値false() throws IOException {
+            assertFalse(
+                    this.target.erase(this.getNotExistingFile(),
+                                      this.getNotExistingFile()));
+        }
+        
+        @Test
+        public void 出力ファイルが存在するファイルだと戻り値false() throws IOException {
+            assertFalse(this.target.erase(this.getExistingFile(),
+                                          this.getExistingFile()));
+        }
+        
+        @Test
+        public void 入力が存在するファイルで出力が存在しないファイルだとtrue() throws IOException {
+            assertTrue(
+                    this.target.erase(this.getExistingFile(),
+                                      this.getNotExistingFile()));
+        }
+        
+        
+        /**
+         * 存在するファイルを取得します。
+         * @return 存在するファイル
+         */
+        private File getExistingFile() {
+            String path = "src/test/resources/FileWithBom.txt";
+            return new File(path);
+        }
+        
+        /**
+         * 存在しないファイルを取得します。
+         * @return 存在しないファイル
+         */
+        private File getNotExistingFile() {
+            String path = "src/test/resources/FileWithoutBom.txt";
+            return new File(path);
+        }
+    }
+
+    /**
+     * BomEraserのisBmメソッド用テストクラス
+     * @author tanabe
+     *
+     */
+    public static class IsBomTest {
+        
+        /**
+         * テスト対象
+         */
+        private BomEraser target;
+        
+        @Before
+        public void before() {
             this.target = new BomEraser();
         }
         
         @Test(expected = IllegalArgumentException.class)
-        public void Nullを渡すと例外発生() {
-            this.target.erase(null);
+        public void 引数がNullだと例外発生() {
+            this.target.isBom(null);
         }
         
         @Test
-        public void 存在しないファイルを渡すと戻り値null() {
-            File file = new File("");
-            assertNull(this.target.erase(file));
+        public void 引数の要素数が0だと戻り値false() {
+            byte[] input = {};
+            assertFalse(this.target.isBom(input));
         }
         
         @Test
-        public void 存在するファイルを渡すと戻り値は非null() {
-            File file = new File("src/test/resources/FileWithBom.txt");
-            assertNotNull(this.target.erase(file));
+        public void 引数がBOM以外から始まる配列だと戻り値false() {
+            byte[] input = {(byte)0x33, (byte)0x34, (byte)0x35, (byte)0x36, (byte)0x37};
+            assertFalse(this.target.isBom(input));
+        }
+        
+        @Test
+        public void 引数がBOMから始まる配列だと戻り値true() {
+            byte[] input = {(byte)0xEF, (byte)0xBB, (byte)0xBF, (byte)0x33, (byte)0x34};
+            assertTrue(this.target.isBom(input));
         }
     }
 }
