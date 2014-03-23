d9285971e67a94e66b2789bdc989b16a6b1d46cd
Tanabe <6isstrong@gmail.com>
2014-03-23 16:34:43 +0900

    パッケージの説明を追加し、デフォルトタスクにjavadoc生成を追加。
    package-info.javaを追加。
    デフォルトタスクにjavadocを追加。

---
+++ TicketReporter/src/main/java/jp/gr/java_conf/ktnet/redmine/client/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Redmineからの情報取得クラス用のパッケージ
+ */
+/**
+ * @author tanabe
+ *
+ */
+package jp.gr.java_conf.ktnet.redmine.client;
---
+++ TicketReporter/src/main/java/jp/gr/java_conf/ktnet/redmine/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Redmine関係のクラス用パッケージ
+ */
+/**
+ * @author tanabe
+ *
+ */
+package jp.gr.java_conf.ktnet.redmine;
